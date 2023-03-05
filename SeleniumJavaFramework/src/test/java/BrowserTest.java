a7ec0bfbd133026fc9db2a205422d47035620ec1
samuelubong <samuelaubong@gmail.com>
2023-03-05 10:55:54 +0100

    First Commit - Project Added

---
+++ SeleniumJavaFramework/src/test/java/BrowserTest.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BrowserTest {
+	
+	public static void main(String[] args) {
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://google.com");
+		
+	}
+
+}
